0df180a1d14d4f134d100282cdf0a2510aaf851b
Jabir Ahmad <www.roman1249@gmail.com>
2025-03-08 15:45:22 +0600

    Refactor package.json and vite.config.js to remove unused dependencies and update Vite version for improved build performance

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import svgr from "vite-plugin-svgr";
 import svgrPlugin from "@svgr/rollup";
 
@@ -16,9 +15,7 @@ export default defineConfig({
     }),
   ],
   resolve: {
-    // alias: {
-    //   "@": path.resolve(__dirname, "./src"),
-    // },
+    
     extensions: [".js", ".jsx", ".json"],
   },
   optimizeDeps: {
